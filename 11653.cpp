2a4357bd2179686ae6a9725f339663779ce1ad86
AntiCaffe <dongjun6288@gmail.com>
2022-01-19 02:25:28 +0900

    https://www.acmicpc.net/problem/1929

--- 11653.cpp
+++ 11653.cpp
@@ -3,37 +3,42 @@ using namespace std;
 class math
 {
 private:
-	int a;
+	int arr[1000001];
+	int a, b;
 public:
-	math(int in)
+	math(int a, int b)
 	{
-		this->a = in;
+		this->a = a;
+		this->b = b;
+		for (int i = 0; i < 1000001; i++)
+		{
+			arr[i] = i;
+		}
+		arr[1] = 0; //1은 소수가 아님으로
 	}
 	void print()
 	{
-		int now = a;
-		int i = 2;
-		while (now >= 2)
+		for (int i = 2; i <= b; i++)
 		{
-			if (now % i == 0)
+			if (arr[i] == 0) continue;
+			for (int j = i*2; j <= b; j+=i)
 			{
-				cout << i << endl;
-				now /= i;
+				arr[j] = 0;
 			}
-			else
-				i++;
 		}
-	} 
-	
-
+		for (int i = a; i <= b; i++)
+		{
+			if (arr[i] != 0)
+				cout << arr[i] << "\n";
+		}
+	}
 };
 int main()
 {
+	int a, b;
+	cin >> a >> b;
 	
-	int a;
-	cin >> a;
-
-	math c = math(a);
+	math c = math(a, b);
 
 	c.print();
 	
